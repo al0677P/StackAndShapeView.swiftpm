74991fa78c1438bd8e584ab92816b08f73ab09a0
Andres Lopez <alopez4578@stu.d214.org>
2023-09-08 10:47:18 -0500

    jk

---
+++ Shapes.swift
@@ -0,0 +1,15 @@
+//
+//  Shapes.swift
+//  StackAndShapeView
+//
+//  Created by Andres E. Lopez on 9/8/23.
+//
+import SwiftUI
+import Foundation
+struct Shapes: View {
+    var body: some View {
+        Circle()
+            .frame()
+        }
+    }
+
