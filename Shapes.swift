d999d9512af3928c161f1909d111c975b8729485
Andres Lopez <alopez4578@stu.d214.org>
2023-09-12 11:18:08 -0500

    created shapes and stretch 1

--- Shapes.swift
+++ Shapes.swift
@@ -9,23 +9,51 @@ import SwiftUI
 struct Shapes: View {
     var body: some View {
         VStack{
+        
+            
             Circle()
-                .frame(width: 100, height: 100)
-                .foregroundColor(.blue)
+                .size(width: 100, height: 100)
+                .background(.blue)
+            
+            Spacer()
             
             Rectangle()
-                .trim(from: 0.5, to: 1.0)
+                .trim(from: 0, to: 0.64)
                 .foregroundColor(.green)
-                
+            
+            Spacer()
             
             Ellipse()
+                .stroke(Color.orange, lineWidth: 5)
+                .background(.brown)
+            
+            Spacer()
             
             Capsule()
+                .foregroundColor(.cyan)
+                .frame(width: 300, height: 100)
+            
+            Spacer()
+            
+            RoundedRectangle(cornerRadius:10)
+                .frame(width: 200, height: 100)
+                .foregroundColor(.red)
+            
+            Rectangle()
+                .size(width: 100, height: 100)
+                .foregroundColor(.blue)
+                .frame(width: 100, height: 100)
+                .overlay(Rectangle() .stroke(.black, lineWidth: 25))
+                
+            
+                
+                
+            }
             
             
-            
+            }
         }
         
-    }
-}
+    
+
 
